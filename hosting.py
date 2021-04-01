a6db5059d4fc14106835040d426b5a615812f9e3
franpessoa <80290528+franpessoa@users.noreply.github.com>
2021-04-01 17:32:30 -0300

    Nome de função mudado

--- hosting.py
+++ hosting.py
@@ -5,11 +5,11 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "LOXLoadingComplete#3954"
+    return "LOXLoadingComplete#3954 https://discord.com/api/oauth2/authorize?client_id=817438170097385523&permissions=8&scope=bot"
 
 def run():
   app.run(host='0.0.0.0',port=8080)
 
-def keep_alive():
+def host_bot():
     t = Thread(target=run)
     t.start()
