e70d091bb9b6bcb85aeb0be31d25eebb35cd579f
franpessoa <franciscomspessoa@gmail.com>
2021-04-05 16:45:21 -0300

    Mudado texto do site

--- hosting.py
+++ hosting.py
@@ -5,7 +5,8 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "LOXLoadingComplete#3954 https://discord.com/api/oauth2/authorize?client_id=817438170097385523&permissions=8&scope=bot"
+    return '''Criado por Francisco Pessoa
+    https://discord.com/api/oauth2/authorize?client_id=817438170097385523&permissions=8&scope=bot'''
 
 def run():
   app.run(host='0.0.0.0',port=8080)
