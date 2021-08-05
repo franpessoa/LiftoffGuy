401d74d5df7750042b94eefcedc20eea9920f8bf
franpessoa <franciscomspessoa@gmail.com>
2021-08-05 14:55:23 -0300

    Adicionado código fo mapa da ISS

---
+++ Mapa-ISS/iss-map.py
@@ -0,0 +1,29 @@
+# Imports #
+import folium
+import requests
+import json
+from time import sleep
+
+# Request para a API #
+url = 'http://api.open-notify.org/iss-now.json'
+issnow = requests.get(url)
+issnow_data = json.loads(issnow.text)
+issnow_list = issnow_data["iss_position"]
+latitude = issnow_list["latitude"]
+longitude = issnow_list["longitude"]
+
+# Fazer o mapa #
+mapa = folium.Map(
+    location=[latitude, longitude],
+    #tiles='Stamen Toner',
+    zoom_start=3.5
+)
+
+# Adicionar marcador #
+folium.Marker(
+    location = [latitude, longitude],
+    popup='<strong>Estação Espacial</strong><br>Latitude: {}\nLongitude: {}'.format(latitude, longitude),
+    tooltip='ISS'
+).add_to(mapa)
+
+mapa.save(r'index.html') # Salva a página HTML #
---
+++ Mapa-ISS/iss-map.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+while :
+do
+	python3 Folium-Mapa.py
+	echo "Mapa atualizado"
+	sleep 15
+done
